e4e1dc0998a2a44bf86284021bc4c46ecc4e6799
Sai Siddhardha Chebrolu <144749543+chebrolusai@users.noreply.github.com>
2024-06-17 14:33:34 -0700

    Create min_stack.cpp

---
+++ neetcode_150/min_stack.cpp
@@ -0,0 +1,62 @@
+/*
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * Implement the MinStack class:
+ * MinStack() initializes the stack object.
+ * void push(int val) pushes the element val onto the stack.
+ * void pop() removes the element on the top of the stack.
+ * int top() gets the top element of the stack.
+ * int getMin() retrieves the minimum element in the stack.
+ * You must implement a solution with O(1) time complexity for each function.
+ *
+ * https://leetcode.com/problems/min-stack/description/
+*/
+
+class MinStack {
+public:
+
+    stack<int> regular;
+    stack<int> min_stack;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        
+        regular.push(val);
+
+        if(min_stack.empty() || min_stack.top() >= val) {
+            min_stack.push(val);
+        }
+
+    }
+    
+    void pop() {
+        
+        int top_value = regular.top();
+
+        regular.pop();
+
+        if(top_value == min_stack.top()) {
+            min_stack.pop();
+        }
+
+    }
+    
+    int top() {
+        return regular.top();
+    }
+    
+    int getMin() {
+        return min_stack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
